25ac44a2218fd9d8a47d3bb8f311b13a01669016
farrar142 <gksdjf1690@gmail.com>
2023-03-04 20:01:05 +0900

    fix:뷰셋 믹스인 추가

--- views.py
+++ views.py
@@ -1,3 +1,5 @@
+from typing import Optional, Any
+from rest_framework import viewsets
 from .utils import MockRequest, RequestModule
 
 
@@ -11,3 +13,14 @@ class UseTokenizedRequestsMixin:
     @property
     def http_authorization(self):
         return self.request.META.get("HTTP_AUTHORIZATION", "")
+
+
+class BaseMixin(viewsets.ModelViewSet):
+    request: MockRequest
+    cached_instance: Optional[Any]
+
+    def get_object(self):
+        if self.cached_instance:
+            return self.cached_instance
+        self.cached_instance = super().get_object()
+        return self.cached_instance
